2a6560799724ec15dde0abb2a956f6bb2315b094
Ben Borgers <borgersbenjamin@gmail.com>
2023-12-14 23:38:27 -0500

    Install Keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import keystatic from "@keystatic/astro";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   adapter: cloudflare(),
-  integrations: [tailwind()],
+  integrations: [tailwind(), react(), keystatic()],
 });
---
+++ keystatic.config.ts
@@ -0,0 +1,27 @@
+// keystatic.config.ts
+import { config, fields, collection } from "@keystatic/core";
+
+export default config({
+  storage: {
+    kind: "github",
+    repo: "benborgers/ben.page",
+  },
+  collections: {
+    posts: collection({
+      label: "Posts",
+      slugField: "title",
+      path: "posts/*",
+      format: { contentField: "content" },
+      schema: {
+        title: fields.slug({ name: { label: "Title" } }),
+        content: fields.document({
+          label: "Content",
+          formatting: true,
+          dividers: true,
+          links: true,
+          images: true,
+        }),
+      },
+    }),
+  },
+});
