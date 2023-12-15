bbd8687826e5c6f98fb34b23a8cd6db7130e91b4
Ben Borgers <borgersbenjamin@gmail.com>
2023-12-14 23:30:50 -0500

    Add Cloudflare router

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
-
+import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "hybrid",
+  adapter: cloudflare(),
   integrations: [tailwind()],
 });
