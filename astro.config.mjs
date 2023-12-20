4d31672f4996001af231d7485be8012546f1841d
Ben Borgers <borgersbenjamin@gmail.com>
2023-12-19 20:43:49 -0500

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import keystatic from "@keystatic/astro";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ben.page",
   output: "hybrid",
   adapter: cloudflare(),
-  integrations: [tailwind(), react(), keystatic()],
+  integrations: [tailwind(), react(), keystatic(), sitemap()],
 });
