70541cd8003a12ae8575880922f164175c5ee1c7
Ben Borgers <borgersbenjamin@gmail.com>
2023-12-31 17:12:30 -0500

    New migration for Ghost

--- migration/ghost.js
+++ migration/ghost.js
@@ -1,4 +1,4 @@
-import ghost from "./ghost-new.json" assert { type: "json" };
+import ghost from "./ghost.json" assert { type: "json" };
 import fs from "fs";
 import { htmlToMdoc } from "./lib.js";
 
@@ -8,6 +8,19 @@ const posts_tags = ghost.db[0].data.posts_tags;
 for (const post of posts.filter((p) => p.status === "published")) {
   const { title, slug, html } = post;
   const technical = !!posts_tags.find((pivot) => pivot.post_id === post.id);
+
+  let mdoc = htmlToMdoc(html);
+
+  mdoc = mdoc.replace(/!\[\]\(__GHOST_URL__(.+?)\)/g, (_, match) => {
+    const filename = match.split("/").pop();
+
+    fs.mkdirSync(`../public/posts/${slug}`, { recursive: true });
+    fs.copyFileSync(
+      match.replace("/content/images", "./ghost-images"),
+      `../public/posts/${slug}/${filename}`
+    );
+    return `![](/posts/${slug}/${filename})`;
+  });
 
   fs.writeFileSync(
     `../posts/${slug}.mdoc`,
@@ -19,7 +32,7 @@ for (const post of posts.filter((p) => p.status === "published")) {
       `unlisted: ${technical}`,
       "---",
       "",
-      htmlToMdoc(html),
+      mdoc,
     ].join("\n")
   );
 }
