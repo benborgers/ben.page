d5fda790690919608e20e5837e6f9143a9adf63e
Ben Borgers <borgersbenjamin@gmail.com>
2024-05-24 22:41:49 -0400

    Try build format: file

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  build: {
+    format: "file",
+  },
   markdown: {
     shikiConfig: {
       theme: "light-plus",
