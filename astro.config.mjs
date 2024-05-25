bf73f3d7949bec72f6306283e445330ec4b0dfcf
Ben Borgers <borgersbenjamin@gmail.com>
2024-05-25 00:56:57 -0400

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://ben.page",
+  integrations: [tailwind(), sitemap()],
   output: "static",
   adapter: vercel(),
   trailingSlash: "never",
