7adcc05dda2c0f15d652ca4a6690f7825420d4fb
Ben Borgers <borgersbenjamin@gmail.com>
2024-05-24 22:47:06 -0400

    Add Vercel plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: "static",
+  adapter: vercel(),
   build: {
     format: "file",
   },
