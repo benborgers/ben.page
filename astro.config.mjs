9e5786bc7f73ef6ed382736c890d37a5d1f3edbd
Ben Borgers <borgersbenjamin@gmail.com>
2024-05-28 09:57:55 -0400

    Remove Vercel server adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
+// import vercel from "@astrojs/vercel/static";
 
 import sitemap from "@astrojs/sitemap";
 
@@ -9,7 +9,7 @@ export default defineConfig({
   site: "https://ben.page",
   integrations: [tailwind(), sitemap()],
   output: "static",
-  adapter: vercel(),
+  // adapter: vercel(),
   trailingSlash: "never",
   markdown: {
     shikiConfig: {
