097aa3913cd16ffdd9e4229289dc9d872e8e4a8e
Ben Borgers <borgersbenjamin@gmail.com>
2024-09-15 18:56:17 -0400

    Install Alpine

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://ben.page",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), alpinejs()],
   trailingSlash: "never",
   markdown: {
     shikiConfig: {
-      theme: "one-light",
-    },
-  },
+      theme: "one-light"
+    }
+  }
 });
