3c2b4aaed76e65fd2d79f59c588e1d3cb97bea88
Ben Borgers <borgersbenjamin@gmail.com>
2024-11-12 23:00:52 -0500

    Remove Alpine.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
-import alpinejs from "@astrojs/alpinejs";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://ben.page",
-  integrations: [tailwind(), sitemap(), alpinejs()],
+  integrations: [tailwind(), sitemap()],
   trailingSlash: "never",
   markdown: {
     shikiConfig: {
